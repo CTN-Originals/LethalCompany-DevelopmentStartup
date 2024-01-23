4f321bf18e98761e008604dbbdecba4d5f738d4e
CTN <info@ctn-originals.com>
2024-01-23 17:25:27 +0100

    1.0.0 [Initial Release]

---
+++ DevelopmentStartup/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace DevelopmentStartup
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.ctnoriginals.DevelopmentStartup";
+        public const string PLUGIN_NAME = "DevelopmentStartup";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
